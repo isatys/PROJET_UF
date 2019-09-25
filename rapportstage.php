b75a544a50fce2e6b7560ac5de1653ea31171d81
isatys <isatys.riviere@free.fr>
2019-09-25 09:20:57 +0200

    git add .

--- rapportstage.php
+++ rapportstage.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="assets/CSS/rapportstage.css">
-    <title>rapport de stage</title>
+    <title>Rapport De Stage</title>
 </head>
 <body>
 <p class="saving">en construction<span>.</span><span>.</span><span>.</span></p>
