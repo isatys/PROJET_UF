4760d40e86feb57084814e5ad462cf02decf45af
isatys <isatys.riviere@free.fr>
2019-09-30 18:53:37 +0200

    blog

--- blogPersonnel.php
+++ blogPersonnel.php
@@ -16,7 +16,7 @@
  <!--navbar-->
      <section class="menu" id="mainNav">
          <div class="container">
-             <nav id="navbar-example" class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color: #533a35">
+             <nav id="navbar-example" class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color: #ff0101">
                      <div class="collapse navbar-collapse" id="navbarResponsive">
                          <ul class="navbar-nav text-uppercase ml-auto">
                              <a class="nav-link " href="experiences.php" style="color: aliceblue">Retour </a>
@@ -27,37 +27,40 @@
      </section>
 <!-- PRESENTATION-->
 <div class='blog'>
-             <p class="text-justify">
-                <div class='pres'>
-                <img src="assets/IMG/darty.png" alt="Darty" class="rounded float-left" id="darty">
-                <h1>1. présentation de l'entreprise : </h1>
-                    Darty est une entreprise qui privilégie les produits électrodoméstiques et d'électronique grand public en France.
+<div class="pres">
+  <img class="align-self-center mr-3" src="assets/IMG/darty.png" alt="Darty"  id="darty">
+  <div class="media-body">  
+    <h5 class="mt-0">1. présentation de l'entreprise :</h5>
+                <p>    Darty est une entreprise qui privilégie les produits électrodoméstiques et d'électronique grand public en France.
                     Les collaborateurs de Darty sont au service et à l'écoute des clients.
                     Au total on peut compter 200 magasins Darty.
                     Grâce Au site, il est possible d'acceder directement au produit et pouvoir ensuite le récuperer en magasin ou le faire livrer.
                     Pour finir il existe 60 centres de service après-ventes et via les assistances téléphoniques spécialisées.
-
+                </p>    
 </div>
-
-                <h1>2. Mon integration dans l'entrerpise : </h1>
-                        Durant le stage, j'ai pu découvrir le commerce et la vente, puisque j'ai participé en fin de stage à la vente 
-                        et de biens mais aussi des services que propose le magasin.
-                        J'ai commencé à prendre des notes pour le restant des 6 semaines afin de récapituler
-                        les tâches et compétences acquises.
-                        Durant le stage j'ai été formé par l'un des vendeurs qui s'occupait du rayon informatique et multimédia.
-                        Au fil des semaines j'ai pu discuter et apprendre des autres vendeurs, qui, eux aussi m'ont beaucoup appris.
-                <h1>3. mon engagement au seins de l'entrerpise : </h1>
-                        Pour commencé j'avais un emploi du temps déjà défini. L'un des chef vendeur Mr Pincet donc, m'a donné plusieurs missions à 
+</div>
+<!--INTEGRATION-->
+<div class="inte">
+  <img class="align-self-center mr-3" src="assets/IMG/commerce.jpg" alt="commerce" id="commerce">
+  <div class="media-body">
+    <h5 class="mt-0">2. Mon integration dans l'entrerpise : </h5>
+    <p> Durant le stage, j'ai pu découvrir le commerce et la vente, puisque j'ai participé en fin de stage à la vente et de biens mais aussi des services que propose le magasin.
+        J'ai commencé à prendre des notes pour le restant des 6 semaines afin de récapituler les tâches et compétences acquises. Durant le stage j'ai été formé par l'un des vendeurs qui s'occupait du rayon informatique et multimédia.
+        Au fil des semaines j'ai pu discuter et apprendre des autres vendeurs, qui, eux aussi m'ont beaucoup appris.</p>
+  </div>
+</div>
+<!-- ENGAGEMENT-->
+<div class="enga">
+  <img class="align-self-center mr-3" src="assets/IMG/enga.jpg" alt="engagement" id="enga">
+  <div class="media-body">
+    <h5 class="mt-0">3. mon engagement au seins de l'entrerpise : </h5>
+        <p>Pour commencé j'avais un emploi du temps déjà défini. L'un des chef vendeur Mr Pincet donc, m'a donné plusieurs missions à 
                         accomplir pour commencé.
                         L'objectif était que je me rende compte des différents secteur des plusiuers employé de chez Darty.
-                        Pour mes deux premières semaines j'ai donc pu m'occuper de:
+                        Pour mes deux premières semaines j'ai donc pu m'occuper de:    
+                        </p>
                         <ul>
-                            <li>   - La mise en rayon ( fiche de prix, ranges les produits par ordre: CAD...)</li>
-                        </ul>
-                        Puis les semaines qui suivent j'ai pu développer mes compétences et partir sur du destockage avec la mise de produits gardés en stock  
-                        enfin dans ma derniere semaines on m'a permsie de pouvoir conseiller les clients pour choisir ou l'achat d'un prouit et
-                        pour finir sur la vente avec les 7 étapes d'une ventes :
-                        <ul>
+                            <li> La mise en rayon ( fiche de prix, ranges les produits par ordre: CAD...)</li>
                             <li> accueillir (Questions ouvertes)</li>
                             <li> Découvertes des besoins </li>
                             <li> Reformulation </li>
@@ -66,18 +69,30 @@
                             <li> Fidélisation du client ( fiche client, carte Darty+)</li>
                             <li> Encaissment</li>
                         </ul>  
-                <h1>  4.Mes connaissances mobilisés lors des missions : </h1>
-                        Ce que j'ai pu mettre en avant grâce à des connaissances était surtout sur la ventes de télephones, ordinateur ou multimédoa ( switch, répeteur wifi... )
+  </div>
+</div><!--CONNAISSANCE-->
+<div class="connai">
+  <img class="align-self-center mr-3" src="assets/IMG/connai.jpg" alt="connaissances" id="connai">
+  <div class="media-body">
+    <h5 class="mt-0"> 4.Mes connaissances mobilisés lors des missions :</h5>
+    <p> Ce que j'ai pu mettre en avant grâce à des connaissances était surtout sur la ventes de télephones, ordinateur ou multimédoa ( switch, répeteur wifi... )
                         puisque ma formation de première année sur l'informatique m'a appris les caractéristiques des ordinateurs ( carte graphique, processeur, stokcage...),
-                        mais aussi dans l'infrastructure (avec les amplificateur de wifi, adresses IP...)
-                <h1>5. les compétences acquises :</h1>
-                        Ce que j'ai beaucoup aimée dans cette entreprise c'est la cohésions d'équipe car le personnel à toujours était disponible
+                        mais aussi dans l'infrastructure (avec les amplificateur de wifi, adresses IP...)</p>
+ </div>
+</div>
+<!-- COMPETENCES-->
+<div class="comp">
+  <img class="align-self-center mr-3" src="assets/IMG/compe.jpg" alt="competences" id="comp">
+  <div class="media-body">
+    <h5 class="mt-0">5. les compétences acquises :</h5>
+    <p>Ce que j'ai beaucoup aimée dans cette entreprise c'est la cohésions d'équipe car le personnel à toujours était disponible
                         et avenant.
                         De plus les vendeurs sont vraiment à l'écoute du client et leurs réponses sont impressionnantes et de qualité, du fait 
                         qu'ils connaissent très bien les produits commercialisés.
                         Ce dont j'ai appris le plus c'est dans la mise en rayon car il y a énormement de missions
-                        à effectuer mais aussi tout la démarhce à suivre pour conseiller un client.
-                </p>
+                        à effectuer mais aussi tout la démarhce à suivre pour conseiller un client.</p>
+  </div>
+</div>
                 </div>
     </body>
 </html>
