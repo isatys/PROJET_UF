a7914a5c388249653e888a53c04bffaa9d26a877
isatys <isatys.riviere@free.fr>
2019-10-07 09:00:16 +0200

    plus footer

--- contact.php
+++ contact.php
@@ -103,25 +103,5 @@
       </section>
   
       <!-- Footer -->
-      <footer class="bg-dark">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-4">
-            </div>
-            <div class="col-md-4">
-              <ul class="list-inline social-buttons">
-                <li class="list-inline-item">
-                  <a href="https://www.facebook.com/isatys.riviere83">
-                    <i class="fab fa-facebook-f"></i>
-                  </a>
-                </li>
-                <li class="list-inline-item">
-                  <a href="https://www.linkedin.com/in/isatys-riviere-9931a9171/">
-                    <i class="fab fa-linkedin-in"></i>
-                  </a>
-              </ul>
-          </div>
-        </div>
-      </footer>
 </body>
 </html>
